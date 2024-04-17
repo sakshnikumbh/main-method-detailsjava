fc4b92301c111659c68b6a27f9d0fa00fefe45fd
sakshnikumbh <nikumbhsakshi362@gmail.com>
2024-04-17 18:26:17 +0530

    widening conversion

---
+++ Widening.java
@@ -0,0 +1,9 @@
+public class Widening {
+    // type casting conversion
+    public static void main(String [] args)
+    {
+        int num1=12;  
+        double num2=num1;      // implicit  Widening conversion 
+        System.out.println(num2);  
+    }
+}
